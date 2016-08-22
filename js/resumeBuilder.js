f302042e761219380f11235ae278693ec6438ac6
Bil Benhamou <bil@propertyfinder.ae>
2016-08-22 19:14:19 +0400

    Chore: coding content

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,61 +12,65 @@ var bio = {
     "welcomeMessage": "Yo",
     "skills": [ "Photoshop", "Illustrator", "Premiere Pro"],
     "biopic": "https://avatars3.githubusercontent.com/u/2975322?v=3&s=460",
-    "display": function () {}
+    "display": function () {
+      if (bio.name !== false) {
+        console.log(bio.name);
+      }
+    }
 };
 
-// Education Object
-var education = {
-        "schools": [{
-            "name": "Leuven",
-            "location": "Brussels",
-            "degree": "N/A",
-            "majors": "The Best",
-            "dates": "2006-2009",
-            "url": "#"
-        }],
-        "onlineCourses": [{
-            "title": "Front-End Web Developer Nanodegree",
-            "school": "Udacity",
-            "dates": "In Progress",
-            "url": "#"
-        }],
-        "display": function () {}
-    };
-
-// Work Object
-var work = {
-        "jobs": [{
-            "employer": "Propertyfinder",
-            "title": "UI Designer",
-            "location": "Dubai",
-            "dates": "In Progress",
-            "description": " User Interface Designer"
-        },
-            {
-                "employer": "Bisnode",
-                "title": "UI Designer",
-                "location": "Brussels",
-                "dates": "2014-2015",
-                "description": "Campaign Architect"
-            },
-            {
-                "employer": "Catalyst IT Limited",
-                "title": "Web Integrator",
-                "location": "Dubai",
-                "dates": "2012-2013",
-                "description": "Integrating a Design for the Open Universities"
-            }],
-        "display": function () {}
-    };
-
-// Projects Object
-var projects = {
-        "projects": [{
-            "title": "Online Portfolio",
-            "dates": "2016",
-            "description": "Online Portfolio Project created for FEND",
-            "images": "Image Url"
-        }],
-        "display": function () {}
-    };
+// // Education Object
+// var education = {
+//         "schools": [{
+//             "name": "Leuven",
+//             "location": "Brussels",
+//             "degree": "N/A",
+//             "majors": "The Best",
+//             "dates": "2006-2009",
+//             "url": "#"
+//         }],
+//         "onlineCourses": [{
+//             "title": "Front-End Web Developer Nanodegree",
+//             "school": "Udacity",
+//             "dates": "In Progress",
+//             "url": "#"
+//         }],
+//         "display": function () {}
+//     };
+//
+// // Work Object
+// var work = {
+//         "jobs": [{
+//             "employer": "Propertyfinder",
+//             "title": "UI Designer",
+//             "location": "Dubai",
+//             "dates": "In Progress",
+//             "description": " User Interface Designer"
+//         },
+//             {
+//                 "employer": "Bisnode",
+//                 "title": "UI Designer",
+//                 "location": "Brussels",
+//                 "dates": "2014-2015",
+//                 "description": "Campaign Architect"
+//             },
+//             {
+//                 "employer": "Catalyst IT Limited",
+//                 "title": "Web Integrator",
+//                 "location": "Dubai",
+//                 "dates": "2012-2013",
+//                 "description": "Integrating a Design for the Open Universities"
+//             }],
+//         "display": function () {}
+//     };
+//
+// // Projects Object
+// var projects = {
+//         "projects": [{
+//             "title": "Online Portfolio",
+//             "dates": "2016",
+//             "description": "Online Portfolio Project created for FEND",
+//             "images": "Image Url"
+//         }],
+//         "display": function () {}
+//     };
