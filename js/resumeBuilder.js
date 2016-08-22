aee9b2807ed97e3ad9cdfe6fd1e3b1763326090c
Bil Benhamou <bil@propertyfinder.ae>
2016-08-22 13:40:26 +0400

    Test: testing material and atom packages

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+  "name" : "Bil Benhamou",
+  "role" : "Front-End Developer/Designer",
+  "contact" : "Bil@benhamou.be",
+  "welcomeMessage" : "Yo",
+  "skills" : "JavaScript",
+}
